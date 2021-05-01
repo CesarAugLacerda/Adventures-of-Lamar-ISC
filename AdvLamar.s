513da59f5273a622537c3bda2b1168e26f9125e5
Felipedbbsb <80411335+Felipedbbsb@users.noreply.github.com>
2021-05-01 18:07:51 -0300

    Add files via upload

---
+++ AdvLamar.s
@@ -0,0 +1,39 @@
+##################################################################
+#  Adventures of Lamar - Projeto final de ISC                    #
+#  2020/2			  			         #
+##################################################################
+# Por favor, comente o codigo com clareza
+
+.data
+#macros
+.include "macro.s"
+
+# personagem para teste
+.include "./Imagens/lamaresq.data"
+.include "./Imagens/lamardir.data"
+.include "./Imagens/lamarbaixo.data"
+.include "./Imagens/lamarcima.data"
+
+# mapa para teste
+.include "./Imagens/MAPA1.data"
+
+# macro para o menu funcional
+.include "menu.s"
+
+# chao para preencher onde o personagem estava
+.include "./Imagens/meiochao.data"
+
+.text
+
+MENU:
+	Menu()
+
+MAIN:
+
+Imprimepersonagem(0xFF008C22, NEXT)
+
+NEXT:
+
+Andapersonagem()
+
+
