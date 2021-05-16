4e128f54343a9c1ac828368c0f1139200514f4e1
César Augusto Lacerda <78516304+Shiir@users.noreply.github.com>
2021-05-16 19:51:11 -0300

    Versão estável

--- menu.s
+++ menu.s
@@ -22,12 +22,14 @@
 .include "./Imagens/Barra_seleçao.data"
 
 .text
+
 #====================MENU=================================
 # Prepara os endereços para printar o menu na tela
-	Impressao(menu1, 0xFF000000, 0xFF100000, 0, MENU_F)
+	ImpressaoF(menu1, 0xFF000000, 0, MENU_F)
 
 # Prepara os enderecos para printar a segunda parte do menu
 MENU_F:
+	Frame(0) #para poder surgir de uma vez a tela do menu
 	Impressao(menu2,0xFF000000, 0xFF100000, 1000, MUSICA)
 
 MUSICA:		# Vazio ate que o menu esteja pronto
@@ -36,44 +38,9 @@ MUSICA:		# Vazio ate que o menu esteja pronto
 MENU_TXT: 
 	Impressao(menu3,0xFF000000, 0xFF100000, 0, SETA)
 #========================================================
-
 SETA:
-	la t0, seta
-	lw t1, 0(t0)
-	lw t2, 4(t0)
-	mul t3, t1, t2
-	addi t0, t0, 8
-	li t4, 0
-	li t6, 19		# Largura x da seta, vai ser usada pra pular linhas.
-	li s1, 0xFF00BF98 		# Endereco onde comeca a imprimir a seta.
-
-
-# So para nao esquecer como funciona:
-# Para imprimir uma imagem em um lugar especifico, preciso da posicao x e y
-# de onde ela vai comecar. Entao conto quantos pixels a imagem tem ate aquele
-# ponto. No caso da resolucao 320x240, seria 320 * (y - 1). Depois somo com o x,
-# que e quantos pixels a mais na proxima linha sao necessarios pra chegar na
-# posicao inicial.
-IMPRIME_SETA: 
-	beq t4, t3, TECLADO
-	lb t5, 0(t0)
-	sb t5, 0(s1)
-	addi t0, t0, 1
-	addi s1, s1, 1
-	addi t4, t4, 1
-	bge t4, t6, PULA_LINHA_SETA
-	j IMPRIME_SETA
-
-# Essa parte e usada para pular para a proxima linha. No caso da seta, ela tem 19px de largura
-# portanto queremos que apos imprimir 19 pixels ele pule pra linha seguinte.
-# Para isso, compara a quantidade de pixels ja impressos com t6, se forem iguais, aumenta 
-# t6 em 19 (em preparacao pra proxima linha), e soma a diferenca entre 320 e a largura da imagem,
-# que no caso e 0x12D, para mostrar a posicao da qual vai comecar na linha seguinte.
-PULA_LINHA_SETA:
-	addi t6, t6, 19
-	addi s1,s1, 0x12D
-	j IMPRIME_SETA
-
+	Impressaopequena(seta, 0xFF00BF98, 0xFF10BF98, 0, 0x12D, TECLADO)
+	
 TECLADO:
 	li s8, 0 		# Nesse trecho, a1 vai definir em que opcao do menu estamos atualmente. 0 = start, 1 = password
 	li s0, 0 		# Zera o contador utilizado nas imagens
@@ -84,10 +51,14 @@ INC:	addi s0, s0, 1		# Incrementa o contador
 	
 #=================================TECLADO===============================
 RECEBE_TECLA: 
+	jal s5, MUSICA1
 	li t1,0xFF200000		# carrega o KDMMIO
+
 	lw t0,0(t1)			# Le bit de Controle Teclado
 	andi t0,t0,0x0001		# mascara o bit menos significativo
-   	beq t0,zero,RETORNA   	   	# Se não há tecla pressionada então vai para Retorno(funça {RETORNA: ret} deve estar no final da pagina do arquivo)
+	
+	
+   	beq t0,zero,RECEBE_TECLA   	   	# Se não há tecla pressionada então vai para Retorno(funça {RETORNA: ret} deve estar no final da pagina do arquivo)
    	lw t2,4(t1)  			# le o valor da tecla
    	
 	li t5, 115			# ascii de "w" para verificar se foi pressionado
@@ -95,113 +66,28 @@ RECEBE_TECLA:
 	li t0, 102			# ascii de "f" para verificar se foi pressionado
 	beq t2, t6, SETA_CIMA		#SETA vai pra cima	
 	beq t2, t5, SETA_BAIXO		#SETA vai pra baixo
-	beq t2, t0, SELEÇAO_MENU	#Entra no lugar desejado	
+	beq t2, t0, SELEÇAO_MENU	#Entra no lugar desejado
+	
 #=========================================================================		
 SETA_CIMA:
 	li s8, 0		# carrega 0 em a1 para indicar que a seta esta em "start"
-	
-	# apaga a seta de baixo antes de printar a de cima
-	APAGA_SETABAIXO:
-	li t1, 0xFF00E518	# carrega o endereco inicial
-	li t2, 266		# quantidade total de pixels a imprimir
-	li t3, 0xFFFFFFFF	# cor branca
-	li t4, 0		# contador de pixels
-	li t6, 19		# contador para pular linha
-	
-LOOP_APAGAB:	beq t4, t2, SETA_CIMA_F		# imprime a seta de baixo quando apagar a de cima
-		beq t4, t6, PULA_LINHA_APAGABAIXO	# pula linha quando chegou no final dos 19px
-		sb t3, 0(t1)				# preenche o pixel de branco
-		addi t4, t4, 1				
-		addi t1, t1, 1
-		j LOOP_APAGAB
-
-PULA_LINHA_APAGABAIXO:
-		addi t6, t6, 19
-		addi t1, t1, 0x12D
-		j LOOP_APAGAB
-	
-	
-	# imprime a seta na posicao de cima
-	SETA_CIMA_F:
-	la t0, seta
-	lw t1, 0(t0)
-	lw t2, 4(t0)
-	mul t3, t1, t2
-	addi t0, t0, 8
-	li t4, 0
-	li t6, 19			
-	li s1, 0xFF00BF98 		
-	
-	IMPRIME_SETACIMA: 
-	beq t4, t3, INC		# retorna e volta a buscar por input do teclado
-	lb t5, 0(t0)
-	sb t5, 0(s1)
-	addi t0, t0, 1
-	addi s1, s1, 1
-	addi t4, t4, 1
-	bge t4, t6, PULA_LINHA_SETACIMA
-	j IMPRIME_SETACIMA
-
-
-	PULA_LINHA_SETACIMA:
-	addi t6, t6, 19
-	addi s1,s1, 0x12D
-	j IMPRIME_SETACIMA
+	apaga_cor(0xFF00E518, 19, 266, 0xFFFFFFFF, 0x12D, IMPRIME_SETA_CIMA )
+	IMPRIME_SETA_CIMA:
+		Impressaopequena(seta, 0xFF00BF98, 0xFF10BF98, 0, 0x12D, INC)
 	
 #----------------------------------------------------------------------------------------------------------------#	
 SETA_BAIXO:
 	li s8, 1		# carrega 1 em a1 para indicar que a seta esta em "password"
+	apaga_cor(0xFF00BF98, 19, 266, 0xFFFFFFFF, 0x12D, IMPRIME_SETA_BAIXO )
+	IMPRIME_SETA_BAIXO:
+		Impressaopequena(seta, 0xFF00E518, 0xFF10E518, 0, 0x12D, INC)
 	
-	APAGA_SETACIMA:
-	li t1, 0xFF00BF98	# carrega o endereco inicial
-	li t2, 266		# quantidade total de pixels a imprimir
-	li t3, 0xFFFFFFFF	# cor branca
-	li t4, 0		# contador de pixels
-	li t6, 19		# contador para pular linha
-	
-LOOP_APAGAC:	beq t4, t2, SETA_BAIXO_F		# imprime a seta de baixo quando apagar a de cima
-		beq t4, t6, PULA_LINHA_APAGACIMA	# pula linha quando chegou no final dos 19px
-		sb t3, 0(t1)				# preenche o pixel de branco
-		addi t4, t4, 1				
-		addi t1, t1, 1
-		j LOOP_APAGAC
-
-PULA_LINHA_APAGACIMA:
-		addi t6, t6, 19
-		addi t1, t1, 0x12D
-		j LOOP_APAGAC
-	
-	SETA_BAIXO_F:
-	la t0, seta
-	lw t1, 0(t0)
-	lw t2, 4(t0)
-	mul t3, t1, t2
-	addi t0, t0, 8
-	li t4, 0
-	li t6, 19			
-	li s1, 0xFF00E518		
-	
-	
-	IMPRIME_SETABAIXO: 
-	beq t4, t3, INC		# retorna e volta a buscar por input do teclado
-	lb t5, 0(t0)
-	sb t5, 0(s1)
-	addi t0, t0, 1
-	addi s1, s1, 1
-	addi t4, t4, 1
-	bge t4, t6, PULA_LINHA_SETABAIXO
-	j IMPRIME_SETABAIXO
-
-
-	PULA_LINHA_SETABAIXO:
-	addi t6, t6, 19
-	addi s1,s1, 0x12D
-	j IMPRIME_SETABAIXO
 #==========================SELEÇÃO Start/Password ======================================
 #s8 = 0 seta esta em start
 #s8 = 1 seta esta em password
  
 SELEÇAO_MENU:
+	jal s5, MUSICA_RESET
 	li t0, 1 
 	beq s8, zero, START		#s8 = 0. vai para START
 	beq s8, t0, PASSWORD		#s8 = 1. vai para PASSWORD
@@ -220,44 +106,69 @@ DELAYHIST:
 
 #+++++++++++++++++++++++++MENU PASSWORD++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++		
 PASSWORD:
-	Impressao(password2, 0xFF000000, 0xFF100000, 0, PASSWORD2) 	#imagem sem asterisco
+	ImpressaoF(password2, 0xFF100000, 0, PASSWORD2) 	#imagem sem asterisco
 	PASSWORD2:
-	Impressao(password1, 0xFF000000, 0xFF100000, 800, BARRA_SELEÇAO) #imagem com asterisco e vai para o teclado
+	Frame(1)				#Troca para frame 1
+	ImpressaoF(password1, 0xFF100000, 800, BARRA_SELEÇAO) #imagem com asterisco e vai para o teclado
 	BARRA_SELEÇAO:
-	Impressaopequena(Barra_seleçao, 0xFF00A53C, 0xFF10A53C, 0, 0x123, TECLADO_PASSWORD)#imprime a barra na seleçao da primeira letra
+	Impressaopequena(Barra_seleçao, 0xFF00A53C, 0xFF10A53C, 0, 0x123, TECLADO_PASSWORD)#imprime a barra na seleÃ§ao da primeira letra
+
 #===========================================VALIDAÇAO SENHA=============
 VALIDADOR_SENHA:
 	lw s5, 0xFF20000C 	#carrega display do KDMMIO, quarta letra
 	#----------------------------------------------------------------#
+	
+	li s7, 5				#lamar começa com 5 de vida(s7) usando uma password
+	
 	#Primeira fase(senha: abba)
 PRIMEIRA_FASE_SENHA:	
-	senha(97, 98, 98, 97,SEGUNDA_FASE_SENHA, START) #vai para START
-	#Segunda fase()		
+	senha(97, 98, 98, 97,SEGUNDA_FASE_SENHA, IMPRESSAO_MAPA1) #se senha correta imprime fase 1
+	#Segunda fase(senha: lmao)		
 SEGUNDA_FASE_SENHA:	
-				
+	#senha(108, 109, 97, 111, TERCEIRA_FASE_SENHA, IMPRESSAO_MAPA2)	#se senha correta imprime fase 2	
+	#Terceira fase(senha: dudu)	
+TERCEIRA_FASE_SENHA:
+	#senha(100, 117, 100, 117, QUARTA_FASE_SENHA, IMPRESSAO_MAPA3)	#se senha correta imprime fase 3
+	#Quarta fase(senha: pokd)
+QUARTA_FASE_SENHA:				
+	#senha(112, 111, 107, 100, QUINTA_FASE_SENHA, IMPRESSAO_MAPA4)	#se senha correta imprime fase 4
+	#Quinta fase(senha: dend)
+QUINTA_FASE_SENHA:
+	#senha(100, 101, 110, 100, SENHA_INCORRETA, IMPRESSAO_MAPA5)	#se senha correta imprime fase 5
 	
-	 j MENU	
+SENHA_INCORRETA:	
+	ret    #retorna para PASSWORD, senha incorreta	
+	
 #=================================SENHAS===============================
 #apaga barra da letra anterior e imprime na proxima
 #senha:(* * * *),(0 1 2 3),(s2, s3, s4, s5)--------> posiçao logica de cada valor sendo respectivamente, apenas asteriscos, posiçao em a1 de cada e registradores onde o valor esta armazendado
 SEGUNDA_LETRA:
 	lw s2, 0xFF20000C 	#carrega display do KDMMIO, primeira letra
-	apaga_cor(0xFF00A53C, 29 ,145,146, 0x123, IMPRIME_SEGUNDA)#apaga barra primeira letra(cor azul)
+	play_sound(72, 1000, 120, 127)
+	apaga_cor(0xFF10A53C, 29 ,145,146, 0x123, IMPRIME_SEGUNDA)#apaga barra primeira letra(cor azul)
 	IMPRIME_SEGUNDA:
 		Impressaopequena(Barra_seleçao, 0xFF00A572, 0xFF10A572 0, 0x123, RECEBE_TECLA_PASSWORD)#imprime a barra na seleçao da segunda letra
 		
 TERCEIRA_LETRA:			#carrega display do KDMMIO, segunda letra
 	lw s3, 0xFF20000C 
-	apaga_cor(0xFF00A572, 29 ,145,146, 0x123, IMPRIME_TERCEIRA)#apaga barra primeira letra(cor azul)
+	play_sound(72, 1000, 120, 127)	
+	apaga_cor(0xFF10A572, 29 ,145,146, 0x123, IMPRIME_TERCEIRA)#apaga barra primeira letra(cor azul)
 	IMPRIME_TERCEIRA:
 		Impressaopequena(Barra_seleçao, 0xFF00A5A7, 0xFF10A5A7 0, 0x123, RECEBE_TECLA_PASSWORD)#imprime a barra na seleçao da terceira letra
 		
 QUARTA_LETRA:
 	lw s4, 0xFF20000C 	##carrega display do KDMMIO, terceira letra
-	apaga_cor(0xFF00A5A7, 29 ,145,146, 0x123, IMPRIME_QUARTA)#apaga barra primeira letra(cor azul)
+	play_sound(72, 1000, 120, 127)
+	apaga_cor(0xFF10A5A7, 29 ,145,146, 0x123, IMPRIME_QUARTA)#apaga barra primeira letra(cor azul)
 	IMPRIME_QUARTA:
 		Impressaopequena(Barra_seleçao, 0xFF00A5DC, 0xFF10A5DC, 0, 0x123, RECEBE_TECLA_PASSWORD)#imprime a barra na seleçao da quarta letra														
 #=================================TECLADO================================
+MENU_TROCAFRAME:
+	ImpressaoF(menu3,0xFF000000, 0, PROXIMO)
+	PROXIMO:
+	Trocaframe(0)
+	Impressao(menu3,0xFF000000,0xFF100000, 0, SETA)
+
 #vai se esperar o jogador apertar uma tecla
 TECLADO_PASSWORD:
 	li s8, 0 		# a1 vai definir a posicao do asterisco(0,1,2,3)    
@@ -279,6 +190,9 @@ LOOP:	lw t0,0(t1)			# Le bit de Controle Teclado
    	#por algum motivo sempre que for reiniciar o BITMAP DISPLAY,caso queira que as teclas sejam digitadas no
    	#display do KDMMIO é necessario desconectar e conectar ele
    	
+   	li t0, 27			# ascii de "esc" para verificar se foi pressionado
+	beq t2, t0, MENU_TROCAFRAME 		#sai do password de volta para o menu
+   	
    	addi s8,s8, 1 			#move posiçao tecla
    	
    	li t0, 1
@@ -291,10 +205,16 @@ LOOP:	lw t0,0(t1)			# Le bit de Controle Teclado
    	beq t0, s8, QUARTA_LETRA	#se a1 determinar que esta na quarta posiçao(3) vai para segunda letra
    	
    	li t0, 4
-   	beq t0,s8, VALIDADOR_SENHA      #quando ultima senha for digitada ele faz a validaçao da senha	
+   	beq t0,s8, VALIDADOR_SENHA_PRE       #quando ultima senha for digitada ele faz a validaçao da senha	
 
    	
-	  	
+VALIDADOR_SENHA_PRE:
+	play_sound(72, 1000, 120, 127)
+	ImpressaoF(password2, 0xFF000000, 0, VALIDADOR) 	#imagem sem asterisco
+	VALIDADOR:
+	Frame(0)
+	jal VALIDADOR_SENHA
+	j PASSWORD	  	
    	
 	
 #=========================================================================
